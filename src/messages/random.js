d06bc6175d13c5daf459704a95037903bed4a116
Christian Storch <ccstorch@gmail.com>
2017-07-04 14:18:14 +0100

    Updates

--- src/messages/random.js
+++ src/messages/random.js
@@ -1,6 +1,8 @@
 module.exports = (slapp) => {
   // Catch-all for any other responses not handled above
   slapp.message('.*', ['mention', 'direct_message'], (msg) => {
-    msg.say("Hmmm, não entendi o que você quis dizer. Mas se você digitar 'ajuda' posso te explicar como eu funciono :smile:");
+    if (Math.random() < 0.4) {
+      msg.say("Hmmm, não entendi o que você quis dizer. \nMas se você digitar 'ajuda' posso te explicar como eu funciono :smile:");
+    }
   })
 }
--- src/messages/thanks.js
+++ src/messages/thanks.js
@@ -1,5 +1,5 @@
 module.exports = (slapp) => {
-  slapp.message(/^(obrigado|obrigada|tks|thx|valeu|show|tkss|tksss|tksssss|agradecido)/i, ['mention', 'direct_message'], (msg) => {
+  slapp.message(/(obrigado|obrigada|tks|thx|valeu|show|tkss|tksss|tksssss|agradecido)/i, ['mention', 'direct_message'], (msg) => {
     msg.say([
       "Que bom que você gostou! :smile:",
       "Fico feliz em poder ajudar",
