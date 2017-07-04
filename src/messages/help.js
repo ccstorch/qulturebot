45da73565fe004782ec8d06fec5a57e058be2fb7
Christian Storch <ccstorch@gmail.com>
2017-07-04 15:09:31 +0100

    Updates

--- src/messages/help.js
+++ src/messages/help.js
@@ -4,11 +4,13 @@ module.exports = (slapp) => {
   Se você precisar de mais ajuda, o ideal é ver esse <https://intercom.help/qulturerocks|artigo completinho>.
   E lembre-se que sou só uma bot.
 
-  **O que é a Qulture.Bot?*
+
+  *O que é a Qulture.Bot?*
   Eu existo para te mandar alertas e notificações sobre tudo o que acontece na Plataforma Qulture.Rocks.
   Portanto vou te avisar quando você receber um pedido de feedback, quando você receber um feedback ou elogio, e coisas do tipo.
 
   No futuro - próximo - vou poder te ajudar a mandar feedbacks e elogios direto do Slack também!
+
 
   *Como faço para sugerir funcionalidades para o Qulture.Bot?*
   Excelente pergunta: mande um email para <mailto:kiko@qulturerocks.com|kiko@qulturerocks.com> que seu feedback vai ser considerado com carinho para as novas versões!
