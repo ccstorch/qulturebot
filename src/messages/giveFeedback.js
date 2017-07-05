6bf029a7c7a0f85d6dc634108a6c0f257a56d609
Christian Storch <ccstorch@gmail.com>
2017-07-05 14:02:38 +0100

    Update

--- src/messages/giveFeedback.js
+++ src/messages/giveFeedback.js
@@ -1,22 +1,41 @@
 module.exports = (slapp) => {
-  slapp.message(/(Dar feedback|feedback)/i, 'direct_message', (msg, text) => {
+  slapp.message('yesno', (msg) => {
     msg.say({
-      attachments: [
-        {
-          text: 'feedback?',
-          fallback: 'Fallback?',
-          callback_id: 'give_feedback_callback',
-          actions: [
-            { name: 'answer', text: 'Dar', type: 'button', value: 'dar' },
-            { name: 'answer', text: 'Pedir',  type: 'button',  value: 'pedir' },
-            { name: 'answer', text: 'Dar Elogio',  type: 'button',  value: 'darElogio' }
-          ]
-        }
-      ]
-    })
-  });
-
-  slapp.action('give_feedback_callback', 'answer', (msg, value) => {
+        text: '',
+        attachments: [
+          {
+            text: '',
+            fallback: 'Yes or No?',
+            callback_id: 'yesno_callback',
+            actions: [
+              { name: 'answer', text: 'Yes', type: 'button', value: 'yes' },
+              { name: 'answer', text: 'No',  type: 'button',  value: 'no' }
+            ]
+          }]
+        })
+  })
+  slapp.action('yesno_callback', 'answer', (msg, value) => {
     msg.respond(msg.body.response_url, `${value} is a good choice!`)
   })
+
+  // slapp.message(/(Dar feedback|feedback)/i, 'direct_message', (msg, text) => {
+  //   msg.say({
+  //     attachments: [
+  //       {
+  //         text: 'feedback?',
+  //         fallback: 'Fallback?',
+  //         callback_id: 'give_feedback_callback',
+  //         actions: [
+  //           { name: 'answer', text: 'Dar', type: 'button', value: 'dar' },
+  //           { name: 'answer', text: 'Pedir',  type: 'button',  value: 'pedir' },
+  //           { name: 'answer', text: 'Dar Elogio',  type: 'button',  value: 'darElogio' }
+  //         ]
+  //       }
+  //     ]
+  //   })
+  // });
+  //
+  // slapp.action('give_feedback_callback', 'answer', (msg, value) => {
+  //   msg.respond(msg.body.response_url, `${value} is a good choice!`)
+  // })
 }
--- src/messages/hello.js
+++ src/messages/hello.js
@@ -1,5 +1,5 @@
 module.exports = (slapp) => {
-  slapp.message(/(Ola|Olá|Oi|Hello|E ai|E aí|Bom dia|Boa tarde|Boa noite|Tem alguém ai|Tem alguem ai|tudo bem|Tudo bem|.)/i, ['direct_mention', 'direct_message'], (msg, text) => {
+  slapp.message(/(Ola|Olá|Oi|Hello|E ai|E aí|Bom dia|Boa tarde|Boa noite|Tem alguém ai|Tem alguem ai|tudo bem|Tudo bem)/i, ['direct_mention', 'direct_message'], (msg, text) => {
     msg.say(["Olá!", "Oi humano!"]);
   });
 }
